6270e1da571ce6cbda4e432e3c582fb827c30ad2
nastefanov <37981705+nastefanov@users.noreply.github.com>
2018-04-02 17:23:26 +0300

    Add files via upload

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,23 @@
+#a)
+x<-c(4,1,1,4)
+#b)
+y<-c(1,4)
+#c)
+#The result is (3,-3,0,0), since y is shorter and is recycled once.
+x-y
+#d)
+s<-c(x,y)
+#e)
+s1<-rep(s, 10)
+length(s1)
+#f)
+s2<-rep(s, times=1, each=3)
+#g)
+seq(from = 7, to = 21)
+s3<-7:21
+#h)
+length(s3)
+
+
+
 
